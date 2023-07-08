61d318fc54563f0bc242b1fca2cc6a128875ba13
Juri Kato <dance.luv48@gmail.com>
2023-07-08 16:51:20 +0900

    still working on bookmarks controller

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,5 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :new]
   def index
     @lists = List.all
   end
@@ -21,6 +22,9 @@ class ListsController < ApplicationController
   end
 
   private
+  def set_list
+    @list = List.find(params[:id])
+  end
 
   def list_params
     params.require(:list).permit(:name)
