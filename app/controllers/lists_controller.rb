ad916d5e3c99e5f7d20222e13e136f312cbb7f32
Juri Kato <dance.luv48@gmail.com>
2023-07-08 16:15:57 +0900

    created list/bookmark controller

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,79 @@
+class ListsController < ApplicationController
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class ListsController < ApplicationController
+
+#   before_action :set_list, only: [:show, :new]
+#   def index
+#     @lists = List.all
+#   end
+
+#   def show
+#   end
+
+#   def new
+#     @list = List.new
+#   end
+
+#   def create
+#     @list = List.new(list_params)
+#     if @list.save
+#       redirect_to list_path(@list)
+#     else
+#       render :new
+#     end
+#   end
+
+#   private
+
+#   def set_list
+#     @list = List.find(params[:id])
+#   end
+
+#   def list_params
+#     params.require(:list).permit(:name, :description)
+#   end
+# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
+  get 'bookmarks/new'
+  get 'bookmarks/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  
-end
+  # resources :movies, only: [:index, :show, :new, :create]
+    resources :lists do
+      resources :bookmarks, only: [:new, :create]
+    end
+    resources :bookmarks, only: [:destroy]
+  end
